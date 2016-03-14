a8739c6ec159c40279da6ada08fe6dfec511a078
卫智熠 <576920016@qq.com>
2016-03-14 15:34:13 +0800

    continue to complete the part of AntColony.

--- AntColony.java
+++ AntColony.java
@@ -9,7 +9,8 @@ public class AntColony extends Main{
 
 
     static int antIDNum;
-    int antID, antPlaceNow, antPlaceDes, antWait, antFood;
+    int antID, antPlaceNow, antPlaceDes, antWait;
+    boolean antFood;
     ArrayList antPath;
 
     public AntColony(){
@@ -31,7 +32,7 @@ public class AntColony extends Main{
         this.antPlaceNow = 0; // 蚂蚁此刻所处的位置
         this.antPlaceDes = 0; //蚂蚁将要去的位置
         this.antWait =  0; // 蚂蚁此刻是否处于等待状态。0代表不需要等待，可以出发，大于零的任意整数 x 代表所需要等待的时间。
-        this.antFood = 0; // 蚂蚁此刻是否拥有从终点取得的食物。有食物，则要在节点释放信息素，没有食物，则不在结点释放信息素。
+        this.antFood = false; // 蚂蚁此刻是否拥有从终点取得的食物。有食物，则要在节点释放信息素，没有食物，则不在结点释放信息素。
         this.antPath = new ArrayList();
 
     }
@@ -64,16 +65,56 @@ public class AntColony extends Main{
         int iC;
 
         /*
-        当前时刻，检查蚁群中每一只蚂蚁的状态。并根据检查结果更新蚂蚁的状态以及地图的信息
+        当前时刻，检查蚁群中每一只蚂蚁的状态。并根据检查结果更新单只蚂蚁的状态。
+        每执行一次当前的方法，则表示经过了一个单位时间。
          */
         for(iC = 0; iC < antNumMax; iC++){
-            antUpdata(dataArrays, antColonies[iC]);
+            dataAntMethod.antUpdata(dataArrays, antColonies[iC]);
+        }
+
+        /*
+
+        如果路径中有信息素，则每个单位时间内-1，如果没有，则一直保持在0，不会变成负数。
+         */
+        for(iC = 0; iC < dataArrays.length; iC++){
+            if(dataArrays[iC].infoMount > 0){
+                dataArrays[iC].infoMount -- ;
+            }
         }
 
     }
 
     public void antUpdata(DataStructure[] dataArrays, AntColony antIndividual){
+        int arrayNum = 0;
 
+        //对于不处于等待状态的蚂蚁进行设置
+        if(antIndividual.antWait == 0){
+
+            arrayNum = dataAntMethod.antDeschoose(dataArrays, antIndividual);
+
+            if(antFood){
+                dataArrays[arrayNum].infoMount = dataArrays[arrayNum].infoMount + 100;
+            }
+
+        }else{
+
+            //
+            antIndividual.antWait -- ;
+        }
+
+    }
+
+    public int antDeschoose(DataStructure[] dataArrays, AntColony antIndividual){
+        int arrayNum = 0;
+        //
+
+        return arrayNum;
+    }
+
+    public ArrayList pointChildSearch(){
+        ArrayList pointChildAddr = new ArrayList();
+
+        return pointChildAddr;
     }
 
     public void antResultShow(DataStructure[] dataArrays){
